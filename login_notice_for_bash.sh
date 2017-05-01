7e8ffe3e5d012d29da2c800fd7acd1783505f6cc
Kuretru <kuretru@gmail.com>
2017-05-01 22:44:31 +0800

    Fixed file format bug

--- login_notice_for_bash.sh
+++ login_notice_for_bash.sh
@@ -14,4 +14,4 @@ user=$(whoami)
 hostname=$(hostname | sed 's/\./_/g')
 ip=$(grep 'Accepted' /var/log/secure | grep ${user} | tail -n 1 |  sed -nr 's/.*[^0-9](([0-9]+\.){3}[0-9]+).*/\1/p')
 
-wget --spider http://sc.ftqq.com/${key}.send?text="${hostname}登录提醒"\&desp="IP地址${ip}，用户名${user}" -q
+wget -q --spider http://sc.ftqq.com/${key}.send?text="${hostname}登录提醒"\&desp="IP地址${ip}，用户名${user}" &
