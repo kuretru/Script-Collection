52291b031576b415eff2de796a10be5e18e4d1ee
呉真 <kuretru@gmail.com>
2017-07-25 11:47:03 +0800

    Updated login_notice_for_bash.sh file

--- login_notice_for_bash.sh
+++ login_notice_for_bash.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #==================================================
-# OS Passed:    CentOS6
+# OS Passed:    CentOS7
 # Description:  bash登录时自动发送登录提醒
 # Author:       kuretru < kuretru@gmail.com >
 # Github:       https://github.com/kuretru/Script-Collection
-# Version:      1.0.170501
+# Version:      1.1.170725
 #==================================================
 
 #Server酱调用密钥
-key=''
+key='Your key'
 
 user=$(whoami)
 hostname=$(hostname | sed 's/\./_/g')
-ip=$(grep 'Accepted' /var/log/secure | grep ${user} | tail -n 1 |  sed -nr 's/.*[^0-9](([0-9]+\.){3}[0-9]+).*/\1/p')
+ip=$(strings /var/log/lastlog | grep -o -P "(\d+\.)(\d+\.)(\d+\.)\d+")
+now=$(date "+%Y.%m.%d_%H.%M.%S")
 
-wget -q --spider http://sc.ftqq.com/${key}.send?text="${hostname}登录提醒"\&desp="IP地址${ip}，用户名${user}"
+wget -q --spider https://sc.ftqq.com/${key}.send?text="${hostname}登录提醒"\&desp="IP地址${ip}，用户名${user}，时间${now}"
